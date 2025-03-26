111bd6a7439f4738a61346c0fa6ba1fe6fab72e2
ibrahem7495 <52601505+ibrahem7495@users.noreply.github.com>
2025-03-26 02:56:31 +0200

    upload

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   APIURL :"https://api.escuelajs.co/api/v1"
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   APIURL :"https://api.escuelajs.co/api/v1"
 
 };
